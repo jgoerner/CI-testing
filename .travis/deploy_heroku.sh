9ebc2884e9b37991aa8f3ccd338d978da9f9fb41
JoshuaGoerner <joshua.goerner@gmail.com>
2017-08-19 18:31:29 +0200

    included Heroku deployment

---
+++ .travis/deploy_heroku.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
+heroku container:push web --app $HEROKU_APP_NAME
